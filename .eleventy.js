7d6ec3c8bc0b77b30ba956104925ebfcd4cf90b1
Fiqri Syah Redha <wibulo27@gmail.com>
2021-08-22 23:17:01 +0700

    add seo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginSEO = require('eleventy-plugin-seo');
+
 module.exports = (eleventyConfig) => {
+	eleventyConfig.addPlugin(pluginSEO, require('./src/_data/seo.json'));
+
 	eleventyConfig.addPassthroughCopy('./src/assets');
 
 	return {
