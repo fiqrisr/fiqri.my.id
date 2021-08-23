4616189c41136beea723f6c5ded476f2da4acf31
Fiqri Syah Redha <wibulo27@gmail.com>
2021-08-23 20:40:36 +0700

    refactor scroll to top button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
-const scrollToTopBtn = document.querySelector('.scroll-to-top');
+const scrollToTopBtn = document.querySelector('#scroll-to-top');
 const rootElement = document.documentElement;
 
 function handleScroll() {
 	const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-	if (rootElement.scrollTop / scrollTotal > 0.2) {
+	if (rootElement.scrollTop / scrollTotal > 0.15) {
 		scrollToTopBtn.classList.add('show-btn');
 	} else {
 		scrollToTopBtn.classList.remove('show-btn');
